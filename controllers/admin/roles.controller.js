0f8cb4a9fdccc0910602aa5095f37b05bec568ec
UIT24730058 <24730058@ms.uit.edu.vn>
2025-06-04 09:24:37 +0700

    xay dung phan phan quyen

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -28,3 +28,27 @@ module.exports.createPost = async (req, res) => {
     await record.save();
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
 };
+
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+    let find = {
+        deleted: false
+    };
+    const records = await Role.find(find);
+
+    res.render('admin/pages/roles/permissions', {
+        pageTitle: 'Phân quyền',
+        records: records,
+    });
+};
+
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+    const permissions = JSON.parse(req.body.permissions);
+    for (const item of permissions) {
+        await Role.updateOne({_id: item.id}, {permissions: item.permissions})
+
+    }
+    res.redirect(req.get('referer'));
+
+};
--- routes/admin/roles.route.js
+++ routes/admin/roles.route.js
@@ -8,6 +8,10 @@ router.get('/', controller.index);
 router.get('/create', controller.create);
 router.post('/create', controller.createPost);
 
+router.get('/permissions', controller.permissions);
+router.patch('/permissions', controller.permissionsPatch);
+
+
 
 
 module.exports = router;
