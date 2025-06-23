9372ed4c3af6144745045b7ac3f13a75970ee27c
UIT24730058 <24730058@ms.uit.edu.vn>
2025-06-23 15:13:51 +0700

    trang gio hang

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,39 @@
 const Cart = require('../../models/cart.model.js');
+const Product = require('../../models/product.model.js');
+
+const productHelper = require('../../helpers/products.js');
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    if (cart.products.length > 0) {
+        for(const item of cart.products){
+            const productId = item.product_id;
+            const productInfo = await Product.findOne({
+                _id: productId,
+                deleted: false
+            }).select('title thumbnail price slug discountPercentage');
+
+            productInfo.priceNew = productHelper.priceNewProduct(productInfo);
+
+            item.productInfo = productInfo;
+            item.totalPrice = item.quantity * productInfo.priceNew;
+        }
+    }
+
+    cart.totalPrice = cart.products.reduce((total, item) => total + item.totalPrice, 0);
+
+    res.render('client/pages/cart/index');
+    pageTitle = 'Giỏ hàng';
+    cartDetail = cart;
+}
+
+
+
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
